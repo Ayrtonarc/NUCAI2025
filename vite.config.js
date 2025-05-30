b8b15f5bb6d06dadaee13e6d299f46a1eb9d65eb
Ayrton Santos <ayrton.arc@gmail.com>
2025-05-30 13:41:44 -0700

    preparando deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/NUCAI2025/', // Cambia esto si tu repo tiene otro nombre
   plugins: [react()],
-})
+});
